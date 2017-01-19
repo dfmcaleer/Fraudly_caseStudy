9eae5efc5a75539165e96913006b5d5ce434643f
Demetria McAleer <dfmcaleer@gmail.com>
2017-01-19 15:49:45 -0800

    make prediction about topic of new data

---
+++ new_data.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
+import cPickle as pickle
+
+from sklearn.cross_validation import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.decomposition import NMF
+
+from sklearn.feature_extraction import text
+from numpy.linalg import lstsq
+from sklearn.cross_validation import train_test_split
+
+import requests
+import json
+import pandas as pd
+
+# from topic_dummies import get_data
+
+def predict(one_line):
+
+    def get_text(cell):
+        return BeautifulSoup(cell, 'html.parser').get_text()
+
+    clean_text = one_line['description'].apply(get_text)
+
+    with open('vectorizer3.pkl') as f:
+        vectorizer = pickle.load(f)
+    with open('model3.pkl') as f:
+        model = pickle.load(f)
+
+    def make_prediction(text):
+        if not text.empty:
+            X = vectorizer.transform(text)
+            prediction = model.predict(X)[0]
+            return prediction
+    prediction = make_prediction(clean_text)
+
+    #df['predicted_topic'] = df['description'].apply(make_prediction)
+
+    return prediction
+
+
+# r = requests.get('http://galvanize-case-study-on-fraud.herokuapp.com/data_point')
+# data=json.loads(r.text)
+# one_line = pd.Series(data,index=data.keys()).to_frame().T
+#
+#
+# print predict(one_line)
